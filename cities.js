d53bfadc25a70b38ec785c012c53f6ce774e5715
David Needham <david.needham@pantheon.io>
2020-09-26 11:13:56 -0500

    All of David's places

--- cities.js
+++ cities.js
@@ -1,7 +1,9 @@
 CITIES =
 [
-   {name: 'Venice', latitude: 45.4408, longitude: 12.3155, radius: 3, fillKey: 'city', date: '1963-10'},
-   // Not really a city, but we define just one type of "POI" (fillKey here) in index.html, you can define more.
-   {name: 'Khao Phing Kan', latitude: 8.2745, longitude: 98.5012, radius: 3, fillKey: 'city', date: '1974-12'},
-   {name: 'San Francisco', latitude: 37.7749, longitude: -122.4194, radius: 3, fillKey: 'city', date: '1985-05'},
+   {name: 'Champaign', latitude: 40.116421, longitude: -88.243385, radius: 3, fillKey: 'home', date: 'Current'},
+   {name: 'Saint Paul', latitude: 46.010521, longitude: -95.681717, radius: 3, fillKey: 'home', date: '2012-01'},
+   {name: 'San Francisco', latitude: 37.7749, longitude: -122.4194, radius: 3, fillKey: 'home', date: '2010-08'},
+   {name: 'New Braunfels', latitude: 29.709881, longitude: -98.119728, radius: 3, fillKey: 'home', date: '2009-12'},
+   {name: 'Peoria', latitude: 40.694592, longitude: -89.590363, radius: 3, fillKey: 'home', date: '2004-08'},
+   {name: 'Ravenden', latitude: 36.236710, longitude: -91.252710, radius: 3, fillKey: 'home', date: 'Past'},
 ]
--- countries.js
+++ countries.js
@@ -1,7 +1,18 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
-    ITA: {fillKey: 'visited'},
     USA: {fillKey: 'visited'},
-    THA: {fillKey: 'visited'},
+    DEU: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
+    JPN: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    IRL: {fillKey: 'visited'},
+    SWE: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    CHE: {fillKey: 'visited'},
+    CAN: {fillKey: 'visited'},
+    SRB: {fillKey: 'visited'},
+    AUT: {fillKey: 'visited'},
+    SVK: {fillKey: 'visited'},
+    ISL: {fillKey: 'visited'},
 }
